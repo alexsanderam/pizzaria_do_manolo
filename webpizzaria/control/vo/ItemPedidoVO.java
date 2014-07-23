1592654afa239afc298e348aa9d64a23c1cf594e
Alexsander Melo <alexsander.a.m@gmail.com>
2014-07-23 17:58:01 -0300

    VOs feitos

---
+++ webpizzaria/control/vo/ItemPedidoVO.java
@@ -0,0 +1,35 @@
+package vo;
+
+public class ItemPedidoVO {
+
+	private PizzaVO pizza;
+	private Integer quantidade;
+	
+	public ItemPedidoVO(){
+		
+	}
+	
+	public ItemPedidoVO(PizzaVO pizza, Integer quantidade){
+		this.pizza = pizza;
+		this.quantidade = quantidade;
+	}
+
+	public PizzaVO obterPizza() {
+		return pizza;
+	}
+
+	public void definirPizza(PizzaVO pizza) {
+		this.pizza = pizza;
+	}
+
+	public Integer obterQuantidade() {
+		return quantidade;
+	}
+
+	public void definirQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+	
+		
+	
+}
---
+++ webpizzaria/control/vo/PagamentoVO.java
@@ -0,0 +1,45 @@
+package vo;
+
+import dominio.EnumFormaDePagamento;
+
+public class PagamentoVO {
+
+	private Long id;
+	private EnumFormaDePagamento formaDePagamento;
+	private Float valorRecebido;
+	
+	public PagamentoVO(){
+		
+	}
+	
+	public PagamentoVO(Long id, EnumFormaDePagamento formaDePagamento, Float valorRecebido){
+		this.id = id;
+		this.formaDePagamento = formaDePagamento;
+		this.valorRecebido = valorRecebido;
+	}
+	
+	public Long obterId() {
+		return id;
+	}
+	
+	public void definirId(Long id) {
+		this.id = id;
+	}
+	
+	public EnumFormaDePagamento obterFormaDePagamento() {
+		return formaDePagamento;
+	}
+	
+	public void definirFormaDePagamento(EnumFormaDePagamento formaDePagamento) {
+		this.formaDePagamento = formaDePagamento;
+	}
+	
+	public Float obterValorRecebido() {
+		return valorRecebido;
+	}
+	
+	public void definirValorRecebido(Float valorRecebido) {
+		this.valorRecebido = valorRecebido;
+	}
+	
+}
